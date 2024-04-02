e3d479208c3cfb7e8e613d2e21eea67b9985b8e9
Laurens van Strijland <lvstrijland@gmail.com>
2024-04-02 15:56:13 +0200

    - Add subscription key to class for subscription key header - Add property types for PHP 7.4 and higher - Bump minimal required PHP version

--- src/Snelstart/Snelstart.php
+++ src/Snelstart/Snelstart.php
@@ -3,20 +3,22 @@
 namespace Snelstart;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Request;
 use Snelstart\Exceptions\SnelstartException;
 
 class Snelstart
 {
-    protected $path = 'https://b2bapi.snelstart.nl/v2/';
-    protected $authPath = 'https://auth.snelstart.nl/b2b/token';
-    protected $apiKey;
-    protected $accessToken;
+    protected string $path = 'https://b2bapi.snelstart.nl/v2/';
+    protected string $authPath = 'https://auth.snelstart.nl/b2b/token';
+    protected ?string $apiKey = null;
+    protected ?string $accessToken = null;
+    protected ?string $subscriptionKey = null;
 
     /**
      * @param string $connectionKey
      * @return Snelstart
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws GuzzleException
      */
     public function generateAccessToken(string $connectionKey): Snelstart
     {
@@ -51,17 +53,18 @@ class Snelstart
     {
         return [
             'Content-Type' => 'application/json',
-            'Ocp-Apim-Subscription-Key' => $this->getApiKey(),
+            'Ocp-Apim-Subscription-Key' => $this->getSubscriptionKey(),
             'Authorization' => 'Bearer ' . $this->getAccessToken()
         ];
     }
 
     /** @throws SnelstartException */
-    public function getApiKey()
+    public function getApiKey(): string
     {
         if (!$this->apiKey) {
             throw new SnelstartException('ApiKey required.', 400);
         }
+
         return $this->apiKey;
     }
 
@@ -71,16 +74,32 @@ class Snelstart
     }
 
     /** @throws SnelstartException */
-    public function getAccessToken()
+    public function getAccessToken(): string
     {
         if (!$this->accessToken) {
             throw new SnelstartException('Generate AccessToken first.', 400);
         }
+
         return $this->accessToken;
     }
 
     public function setAccessToken(string $accessToken): void
     {
         $this->accessToken = $accessToken;
+    }
+
+    /** @throws SnelstartException */
+    public function getSubscriptionKey(): string
+    {
+        if (!$this->subscriptionKey) {
+            throw new SnelstartException('Subscription key is required.', 400);
+        }
+
+        return $this->subscriptionKey;
+    }
+
+    public function setSubscriptionKey(string $subscriptionKey): void
+    {
+        $this->subscriptionKey = $subscriptionKey;
     }
 }
